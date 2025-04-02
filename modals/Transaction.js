9eca2879b978027927ffd12966c735002c919dfd
CapitalOne444 <onecapital283@gmail.com>
2025-04-02 08:17:05 +0000

    transaction

--- modals/Transaction.js
+++ modals/Transaction.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose')
 
 const TransactionSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    utr: String,
     amount: Number,
     mode: String,
     transactionId: String,
-    created_at : String,
+    created_at : { type: Date, default: Date.now },
     status: {
         type: String,
         default: "Pending"
     },
-    updated_at : String
+    updated_at : { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model("Transaction", TransactionSchema)
