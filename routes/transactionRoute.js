7a5e98b31e52e2c75518e27f9cc7774da5661c97
CapitalOne444 <onecapital283@gmail.com>
2025-04-02 08:23:27 +0000

    transaction

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -2,18 +2,18 @@ const express = require("express")
 const Transaction = require('../modals/Transaction')
 const router = express.Router()
 
-router.post('/transaction',async (req, res)=>{
+router.post('/transaction', async (req, res) => {
    const newTransaction = new Transaction(req.body)
    await newTransaction.save()
    res.status(201).send("Transaction Created Successfully!")
 })
 
-router.get('/transaction', async (req, res)=>{
+router.get('/transaction', async (req, res) => {
    const alltransactions = await Transaction.find().populate('userId', 'name');
    res.status(200).send(alltransactions)
 })
 
-router.put('/transaction/:id', async(req, res)=>{
+router.put('/transaction/:id', async (req, res) => {
    const transaction = await Transaction.findById(req.params.id)
 
    transaction.userId = req.body.userId || transaction.userId
@@ -27,8 +27,28 @@ router.put('/transaction/:id', async(req, res)=>{
    await transaction.save()
    res.status(201).send("Transaction Updated Successfully!")
 })
+router.get('/transaction/user/:id', async (req, res) => {
+   try {
+      const { userId } = req.params.id;
+      if (!userId) {
+         return res.status(400).json({ message: "User ID is required!" });
+      }
 
-router.delete('/transaction/:id', async(req, res)=>{
+      const transactions = await Transaction.find({ userId }).populate('userId', 'name');
+      if (transactions.length === 0) {
+         return res.status(404).json({ message: "No transactions found for this user." });
+      }
+
+      res.status(200).json(transactions);
+
+   } catch (error) {
+      res.status(500).json({ message: "Error fetching transactions", error });
+   }
+
+
+})
+
+router.delete('/transaction/:id', async (req, res) => {
    await Transaction.findByIdAndDelete(req.params.id)
    res.status(200).send("Transaction Deleted Successfully")
 })
