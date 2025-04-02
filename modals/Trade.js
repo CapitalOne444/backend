2403da14f5f641902bdc7842b863418431948c85
CapitalOne444 <onecapital283@gmail.com>
2025-04-02 21:49:50 +0530

    Update Trade.js

--- modals/Trade.js
+++ modals/Trade.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose')
 
+const TradeItemSchema = new mongoose.Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true }, // Unique ID for each trade object
+    averagePrice: Number, 
+    type:String,
+    status: String,
+    investmentAmount: Number, 
+    quantity: Number,
+    created_at: { type: Date, default: Date.now }
+});
+
 const TradeSchema = new mongoose.Schema({
     tradeId: String,
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     indexes: String,
     stockName: String,
     option: String,
-    tradeList: Array, 
-    
+    tradeList: [TradeItemSchema]  // Array of trade objects with unique ID    
    
    
 })
