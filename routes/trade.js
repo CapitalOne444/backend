7262e4ec7eaf7b6ab85c9084e784ae313ad1e291
CapitalOne444 <onecapital283@gmail.com>
2025-04-03 08:16:29 +0000

    trade

--- routes/trade.js
+++ routes/trade.js
@@ -55,8 +55,8 @@ router.put("/trade/add/:id", async (req, res) => {
             quantity,
             created_at: new Date(),
         };
-        trade.mainTrade.investmentAmount = type == "buy" ? trade.mainTrade.investmentAmount + newTrade.investmentAmount : trade.mainTrade.investmentAmount - newTrade.investmentAmount
-        trade.mainTrade.quantity = type == "buy" ? trade.mainTrade.quantity + newTrade.quantity : trade.mainTrade.quantity - newTrade.quantity
+        trade.mainTrade.investmentAmount = newTrade.type == "buy" ? trade.mainTrade.investmentAmount + newTrade.investmentAmount : trade.mainTrade.investmentAmount - newTrade.investmentAmount
+        trade.mainTrade.quantity = newTrade.type == "buy" ? trade.mainTrade.quantity + newTrade.quantity : trade.mainTrade.quantity - newTrade.quantity
 
         trade.tradeList.push(newTrade);
         await trade.save();
